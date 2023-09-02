aba092d509906799135b0322dcbc5d3b9f18815a
eliasfreeman <oladimejiboluwaji@gmail.com>
2023-09-02 01:40:29 +0100

    new

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,11 @@ import navBg from "../assets/THJJRUhNlec.png";
 function Nav({ children }) {
   return (
     <nav className="bg-gradient-to-b from-slate-900/70 to-slate-900/90 to h-52 w-screen bg-no-repeat bg-cover bg-center relative flex justify-center items-center ">
-      <img src={navBg} className="absolute w-full h-full object-cover"></img>
+      <img
+        src={navBg}
+        className="absolute w-full h-full object-cover"
+        alt="background"
+      ></img>
       {children}
     </nav>
   );
--- src/components/VideoImg.jsx
+++ src/components/VideoImg.jsx
@@ -3,7 +3,7 @@ import videoImg from "../assets/Icon.svg";
 function VideoImg() {
   return (
     <div className="flex flex-col items-center space-y-4">
-      <img src={videoImg} alt="" />
+      <img src={videoImg} alt="video pic" />
       <h3 className="text-gray-400 text-2xl">Start exploring</h3>
     </div>
   );
