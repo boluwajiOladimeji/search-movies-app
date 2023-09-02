c3d425dd7a5d5fc12f494519b8f646ad52fab085
eliasfreeman <oladimejiboluwaji@gmail.com>
2023-09-02 02:08:01 +0100

    new

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,13 @@
+import movieBg from "../assets/movie-bg.jpg";
+
 function Nav({ children }) {
   return (
-    <nav className="bg-gradient-to-b from-slate-900/40 to-slate-900/60 to h-52 w-screen bg-no-repeat bg-cover bg-center relative flex justify-center items-center ">
+    <nav className="bg-gradient-to-b from-slate-900/90 to-slate-900 to h-52 w-screen bg-no-repeat bg-cover bg-center relative flex justify-center items-center ">
+      <img
+        src={movieBg}
+        className="absolute w-full h-full object-cover"
+        alt="background"
+      ></img>
       {children}
     </nav>
   );
